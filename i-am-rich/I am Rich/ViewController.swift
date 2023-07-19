c29f90568a0712ec27597e547dbb5fe38a2a8c2c
Pichborith Kong <pichborith@gmail.com>
2023-07-19 09:10:47 +0700

    add I am Rich Project

---
+++ i-am-rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Pichborith Kong on 7/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
