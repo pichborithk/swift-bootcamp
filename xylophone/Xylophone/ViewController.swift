7f01136236ed8fa7140463ffc60b424458573fb1
Pichborith Kong <pichborith@gmail.com>
2023-08-19 00:57:39 +0700

    complete xylophone project

--- xylophone/Xylophone/ViewController.swift
+++ xylophone/Xylophone/ViewController.swift
@@ -7,18 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    
     @IBAction func keyPressed(_ sender: UIButton) {
+//        playSound(sender.titleLabel?.text)
+        playSound(soundName: sender.currentTitle!)
+//        playSound(sender.title(for: .normal))
+//        var originalColor = sender.backgroundColor
+//        sender.backgroundColor = UIColor.red.withAlphaComponent(0.5)
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+//           print("This message is delayed")
+//            sender.backgroundColor = originalColor
+            sender.alpha = 1
+        }
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
         
     }
     
-    
-
 }
 
