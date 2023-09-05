3aa854a3580353459b66246d035366a14c91f9e8
Pichborith Kong <pichborith@gmail.com>
2023-09-05 00:20:59 -0700

    finish egg timer project

--- egg-timer/Egg Timer/ViewController.swift
+++ egg-timer/Egg Timer/ViewController.swift
@@ -7,26 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
     //    let softTimer = 5
     //    let mediumTimer = 7
     //    let hardTimer = 12
     
-    let eggTimer = ["Soft": 3, "Medium": 420, "Hard": 720]
-    
-    var secondsRemaining = 60
-    
+    let eggTimer = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var totalTime = 0
+    var secondsPassed = 0
     var timer = Timer()
+    
+    var player: AVAudioPlayer!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle!
         
         timer.invalidate()
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
 //        switch hardness {
 //        case "Soft":
 //            print(softTimer)
@@ -39,21 +44,29 @@ class ViewController: UIViewController {
 //        }
         
 //        print(eggTimer[hardness]!)
-        secondsRemaining = eggTimer[hardness]!
+        totalTime = eggTimer[hardness]!
+        
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds")
-            secondsRemaining -= 1
+        if totalTime > secondsPassed {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
         } else {
             timer.invalidate()
             titleLabel.text = "DONE!"
+            playSound()
         }
         
     }
     
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
 
 }
