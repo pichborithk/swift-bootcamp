c79939eb26acf20d59538a76c035f2095c381101
Pichborith Kong <pichborith@gmail.com>
2023-09-09 21:36:51 -0700

    add quizzler multiple choice version

---
+++ quizzler (multiple choice)/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Pichborith Kong on 9/8/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answers: [String]
+    let correctAnswer: String
+    
+    init(q: String, a: [String], correctAnswer: String) {
+        self.text = q
+        self.answers = a
+        self.correctAnswer = correctAnswer
+    }
+}
