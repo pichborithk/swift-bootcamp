50e6746e98e010f2bfd30a4df08e53918af753e5
Pichborith Kong <pichborith@gmail.com>
2023-09-11 08:27:10 -0700

    finish BMI calculator project

--- BMI-calculator/BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI-calculator/BMI Calculator/Controllers/CalculatorBrain.swift
@@ -15,13 +15,23 @@ struct CalculatorBrain {
     mutating func calculateBMI(height: Float, weight: Float) {
         let bmiValue = weight / pow(height, 2)
         if bmiValue < 18.5 {
-            bmi = BMI(value: bmiValue, advice: "Eat more pies", color: Color(#colorLiteral()))
+            bmi = BMI(value: bmiValue, advice: "Eat more pies", color: UIColor(hex: "#82A0D8")!)
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle", color: UIColor(hex: "#A8DF8E")!)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies", color: UIColor(hex: "#C63D2F")!)
         }
-    
-    
     }
     
     func getBMIValue() -> String {
         return String(format: "%.1f", bmi?.value ?? 0.0)
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? ""
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? UIColor.white
     }
 }
