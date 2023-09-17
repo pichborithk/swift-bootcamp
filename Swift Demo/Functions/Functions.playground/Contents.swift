91725c85e6a538c920306f06f0f41cb69f055826
Pichborith Kong <pichborith@gmail.com>
2023-09-17 00:59:50 -0700

    update swift demo

--- Swift Demo/Functions/Functions.playground/Contents.swift
+++ Swift Demo/Functions/Functions.playground/Contents.swift
@@ -1,8 +1,8 @@
-func a () {
+func a() {
     print("a")
 }
 
-func a (b: String) {
+func a(b: String) {
     print(b)
 }
 
@@ -10,15 +10,22 @@ a()
 
 a(b: "b")
 
-func b (_ c: String) {
+func b(_ c: String) {
     print(c)
 }
 
 b("c")
 
-func c (d: Int) -> Int {
+func c(d: Int) -> Int {
     return d + 1
 }
 
 let d = c(d: 1)
 
+func f(g: Int = 0) -> Int{
+    return g + 1
+}
+
+print(f())
+
+print(f(g: 2))
