70c9e737666ee8a3b0d146b7db5b2cde8f58d05d
Pichborith Kong <pichborith@gmail.com>
2023-09-19 16:22:21 -0700

    add loops demo

--- Swift Demo/Loops/Loops.playground/Contents.swift
+++ Swift Demo/Loops/Loops.playground/Contents.swift
@@ -1,3 +1,19 @@
-import UIKit
+let array = ["a", "b", "c", "d", "e"]
+let dict = ["a": 0, "b": 1, "c": 2, "d": 3, "e": 4]
 
-var greeting = "Hello, playground"
+for char in array {
+    print(char)
+}
+
+for item in dict {
+    print(item.key)
+    print(item.value)
+}
+
+for n in 0 ..< 5 {
+    print(n)
+}
+
+for n in 0 ... 4 {
+    print(n)
+}
