651c99f11753ed9d0824a3894d7d3e9157649cee
Pichborith Kong <pichborith@gmail.com>
2024-03-25 15:45:36 -0700

    update Structures demo

--- Swift Demo/Swift Demo.playground/Pages/Structures.xcplaygroundpage/Contents.swift
+++ Swift Demo/Swift Demo.playground/Pages/Structures.xcplaygroundpage/Contents.swift
@@ -1,69 +1,71 @@
-struct Town {
-    let name = "a"
-    var citizens = [1, 2, 3]
-    var resources = ["a": 1, "b": 2]
+// Structure Concepts with Static and Access Modifiers Demonstration
 
-    func fortify() {
-        print("b")
+// Access Modifiers:
+
+// public: Allows entities to be accessed from any source file.
+// private: Restricts the use of an entity to its own defining source file.
+// internal: Allows entities to be accessed within the defining module.
+
+// 1. Define a Structure with Access Modifiers
+public struct Person {
+    // Public properties
+    public var name: String
+    public var age: Int
+    private var address: String
+
+    // Computed Property
+    public var description: String {
+        return "\(name) is \(age) years old and lives at \(address)."
+    }
+
+    // Method with Internal Access Modifier
+    func introduce() {
+        print("Hello, my name is \(name) and I am \(age) years old.")
+    }
+
+    // Static Property or Type Property
+    public static var count: Int = 0
+
+    // Static Method or Type Method
+    public static func increaseCount() {
+        count += 1
+    }
+
+    // Initializer
+    public init(name: String, age: Int, address: String) {
+        self.name = name
+        self.age = age
+        self.address = address
+        Person.increaseCount()
+    }
+
+    // Mutate Method to modify Properties
+    mutating func changeName(newName: String) {
+        name = newName
     }
 }
 
-var myTown = Town()
+// 2. Create Instances of the Structure
+let person1 = Person(name: "John", age: 30, address: "123 Main St")
+let person2 = Person(name: "Jane", age: 25, address: "456 Elm St")
 
-print(myTown.name)
-print(myTown.citizens)
-myTown.fortify()
+// 3. Access Properties and Call Methods
+print("Person 1:", person1.description)
+print("Person 2:", person2.description)
 
-struct Car {
-    let color: String
-    let tires = 4
+person1.introduce()
+person2.introduce()
 
-    init(color: String) {
-        self.color = color
-    }
-}
+// 4. Access Static Property and Call Static Method
+print("Number of Persons:", Person.count)
 
-var myCar = Car(color: "red")
-myCar.tires
-myCar.color
+// 5. Modify Properties (Not possible for address due to private access modifier)
+var person3 = person1
+person3.name = "Alice"
+person3.age = 28
+// person3.address = "789 Oak St"  // This would cause a compile-time error due to the private access modifier
 
-struct Bike {
-    var color: String
-    let tires: Int
+print("Person 3:", person3.description)
 
-    mutating func changeColor(newColor: String) {
-        color = newColor
-    }
-}
-
-var myBike = Bike(color: "blue", tires: 2)
-myBike.tires
-myBike.color
-myBike.changeColor(newColor: "Yellow")
-myBike.color
-
-var bike1 = Bike(color: "pink", tires: 2)
-var bike2 = bike1
-
-bike1.changeColor(newColor: "green")
-bike2.changeColor(newColor: "orange")
-print(bike1.color)
-print(bike2.color)
-
-// Type Property
-
-struct Constants {
-    let instanceProperty = "abc" // Instance Property
-    static let typeProperty = "123"
-    
-    static func typeMethod() {
-        print("Type Method has been called...")
-    }
-}
-
-var myConstant = Constants()
-
-// myConstant can not access Type Property
-
-print(Constants.typeProperty) // This is the way to access Type Property
-Constants.typeMethod()
+// 6. Structures are Value Types
+print("Person 1:", person1.description)
