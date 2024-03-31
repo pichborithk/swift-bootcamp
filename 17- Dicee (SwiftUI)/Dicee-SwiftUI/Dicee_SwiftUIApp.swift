f54a686a252ff80624b8ce0e1243188b594e63a0
Pichborith Kong <pichborith@gmail.com>
2024-03-31 14:05:06 -0700

    add dicee app with swithui

---
+++ 17- Dicee (SwiftUI)/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Pichborith Kong on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
